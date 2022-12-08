63699544ec2f926662d080999661c41bdb5723f1
sheila alamo <sheilaalamoalarcon@gmail.com>
2022-12-08 02:53:49 +0100

    task: add component header, and add ticket button

---
+++ time-out-front/components/header/header.component.tsx
@@ -0,0 +1,10 @@
+import styles from "./header.module.scss";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Time Out</h1>
+      <img src="" alt="user-icon" />
+    </div>
+  );
+}
